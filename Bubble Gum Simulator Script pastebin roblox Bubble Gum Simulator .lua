9d12a968abaa4d68a3af2de89caa356920aee286
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-28 02:52:24 +0100

    commit zero

---
+++ Bubble Gum Simulator Script pastebin roblox Bubble Gum Simulator .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet(("http://roguefamily.com/script/streleziaBGSbeta/script"),true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							Script with auto-farm, island discovery and auto-egg
+						
+]]
